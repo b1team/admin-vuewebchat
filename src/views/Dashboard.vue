40d6326fb31a8f9199807cd8c1a21469ee617b20
Falcol <bvngoc2000@gmail.com>
2021-06-26 14:46:52 +0700

    fix dialog bug

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -27,144 +27,141 @@
 		</v-row>
 		<v-row>
 			<v-col cols="12" sm="6" md="6">
-				<v-list subheader class="table">
-					<v-subheader><h2>Người dùng mới nhất</h2></v-subheader>
+				<h2>Người dùng</h2>
+				<v-virtual-scroll
+					:items="recent"
+					:item-height="80"
+					height="500"
+				>
+					<template v-slot:default="{ item }">
+						<v-list-item class="t-color">
+							<v-list-item-avatar>
+								<v-img
+									:alt="`${item.title} avatar`"
+									:src="item.avatar"
+								></v-img>
+							</v-list-item-avatar>
 
-					<v-list-item
-						class="t-color"
-						v-for="chat in recent"
-						:key="chat.title"
-					>
-						<v-list-item-avatar>
-							<v-img
-								:alt="`${chat.title} avatar`"
-								:src="chat.avatar"
-							></v-img>
-						</v-list-item-avatar>
+							<v-list-item-content>
+								<v-list-item-title
+									v-text="item.title"
+								></v-list-item-title>
+							</v-list-item-content>
 
-						<v-list-item-content>
-							<v-list-item-title
-								v-text="chat.title"
-							></v-list-item-title>
-						</v-list-item-content>
-
-						<v-list-item-icon>
-							<v-btn
-								@click="dialogActive = !dialogActive"
-								class="ma-2 btn-action"
-								outlined
-							>
-								{{ chat.active ? "Chặn" : "Bỏ chặn" }}
-							</v-btn>
-						</v-list-item-icon>
-						<v-dialog v-model="dialogActive" max-width="290">
-							<v-card>
-								<v-card-title class="headline">
-									{{
-										chat.active
-											? "Bạn muốn chặn người dùng ?"
-											: "Bạn muốn bỏ chặn người dùng ?"
-									}}
-								</v-card-title>
-								<v-card-actions>
-									<v-spacer></v-spacer>
-
-									<v-btn
-										class="btn-unaction"
-										text
-										@click="dialogActive = false"
-									>
-										Không
-									</v-btn>
-
-									<v-btn
-										class="btn-action"
-										text
-										@click="activeUser(chat.id)"
-									>
-										Đồng ý
-									</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-dialog>
-					</v-list-item>
-				</v-list>
+							<v-list-item-icon>
+								<v-btn
+									@click="setupActiveUser(item)"
+									class="ma-2 btn-action"
+									outlined
+								>
+									{{ item.active ? "Chặn" : "Bỏ chặn" }}
+								</v-btn>
+							</v-list-item-icon>
+						</v-list-item>
+					</template>
+				</v-virtual-scroll>
 
 				<v-divider></v-divider>
 			</v-col>
+
 			<v-col cols="12" sm="6" md="6">
-				<v-list subheader class="table">
-					<v-subheader><h2>Phòng mới nhất</h2></v-subheader>
+				<h2>Phòng</h2>
+				<v-virtual-scroll
+					:items="recentRoom"
+					:item-height="80"
+					height="500"
+				>
+					<template v-slot:default="{ item }">
+						<v-list-item class="t-color">
+							<v-list-item-avatar>
+								<v-img
+									v-if="item.avatar"
+									:alt="`${item.room_name} avatar`"
+									:src="item.avatar"
+									@error="item.avatar = ''"
+								></v-img>
+								<v-avatar
+									v-if="!item.avatar"
+									color="blue"
+									size="40"
+								>
+									<span class="white--text headline">{{
+										textAvatar(item.room_name)
+									}}</span>
+								</v-avatar>
+							</v-list-item-avatar>
 
-					<v-list-item
-						class="t-color"
-						v-for="room in rooms.five_rooms"
-						:key="room.id"
-					>
-						<v-list-item-avatar>
-							<v-img
-								v-if="room.avatar"
-								:alt="`${room.room_name} avatar`"
-								:src="room.avatar"
-								@error="room.avatar = ''"
-							></v-img>
-							<v-avatar
-								v-if="!room.avatar"
-								color="blue"
-								size="40"
-							>
-								<span class="white--text headline">{{
-									textAvatar(room.room_name)
-								}}</span>
-							</v-avatar>
-						</v-list-item-avatar>
+							<v-list-item-content>
+								<v-list-item-title
+									v-text="item.room_name"
+								></v-list-item-title>
+							</v-list-item-content>
 
-						<v-list-item-content>
-							<v-list-item-title
-								v-text="room.room_name"
-							></v-list-item-title>
-						</v-list-item-content>
-
-						<v-list-item-icon>
-							<v-btn
-								@click="dialogActiveRoom = !dialogActiveRoom"
-								class="ma-2 btn-action"
-								outlined
-							>
-								Xóa phòng
-							</v-btn>
-						</v-list-item-icon>
-						<v-dialog v-model="dialogActiveRoom" max-width="290">
-							<v-card>
-								<v-card-title class="headline">
-									Bạn muốn xóa phòng ?
-								</v-card-title>
-								<v-card-actions>
-									<v-spacer></v-spacer>
-
-									<v-btn
-										class="btn-unaction"
-										text
-										@click="dialogActiveRoom = false"
-									>
-										Không
-									</v-btn>
-
-									<v-btn
-										class="btn-action"
-										text
-										@click="deleteRoom(room.id)"
-									>
-										Đồng ý
-									</v-btn>
-								</v-card-actions>
-							</v-card>
-						</v-dialog>
-					</v-list-item>
-				</v-list>
+							<v-list-item-icon>
+								<v-btn
+									@click="setupActiveRoom(item)"
+									class="ma-2 btn-action"
+									outlined
+								>
+									Xóa phòng
+								</v-btn>
+							</v-list-item-icon>
+						</v-list-item>
+					</template>
+				</v-virtual-scroll>
 				<v-divider></v-divider>
 			</v-col>
 		</v-row>
+
+		<v-dialog v-model="dialogActive" max-width="290">
+			<v-card>
+				<v-card-title class="headline">
+					{{
+						activeTitle
+							? "Bạn muốn chặn người dùng ?"
+							: "Bạn muốn bỏ chặn người dùng ?"
+					}}
+				</v-card-title>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+
+					<v-btn
+						class="btn-unaction"
+						text
+						@click="dialogActive = false"
+					>
+						Không
+					</v-btn>
+
+					<v-btn class="btn-action" text @click="activeUser">
+						Đồng ý
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+
+		<v-dialog v-model="dialogActiveRoom" max-width="290">
+			<v-card>
+				<v-card-title class="headline">
+					Bạn muốn xóa phòng ?
+				</v-card-title>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+
+					<v-btn
+						class="btn-unaction"
+						text
+						@click="dialogActiveRoom = false"
+					>
+						Không
+					</v-btn>
+
+					<v-btn class="btn-action" text @click="deleteRoom">
+						Đồng ý
+					</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
 	</v-app>
 </template>
 
@@ -183,15 +180,19 @@ export default {
 			info: [],
 			isMobile: false,
 			recent: [],
+			recentRoom: [],
 			dialogActive: false,
 			dialogActiveRoom: false,
+			activeUserId: null,
+			activeRoomId: null,
+			activeTitle: null,
 		};
 	},
-	created() {
-		this.getUserData();
-		this.loadUsers();
-		this.messagesCount();
-		this.loadRooms();
+	async created() {
+		await this.getUserData();
+		await this.loadUsers();
+		await this.messagesCount();
+		await this.loadRooms();
 	},
 	mounted() {
 		this.isMobile = window.innerWidth < 500 ? true : false;
@@ -206,25 +207,24 @@ export default {
 			});
 		},
 		loadUsers: function() {
-			this.$store.dispatch("ad_loadUsers").then(() => {
-				this.users = this.$store.getters.ad_users;
-				const data = {
-					detail: "Người dùng",
-					count: this.users.count,
-					icon: "mdi-account-circle",
-				};
-				this.info.push(data);
+			this.$store.dispatch("ad_loadUsers");
+			this.users = this.$store.getters.ad_users;
+			const data = {
+				detail: "Người dùng",
+				count: this.users.count,
+				icon: "mdi-account-circle",
+			};
+			this.info.push(data);
 
-				for (const user of this.users.five_users) {
-					const info = {
-						active: user.active,
-						avatar: user.avatar,
-						title: user.username,
-						id: user.user_id,
-					};
-					this.recent.push(info);
-				}
-			});
+			for (const user of this.users.users) {
+				const info = {
+					active: user.active,
+					avatar: user.avatar,
+					title: user.username,
+					id: user.user_id,
+				};
+				this.recent.push(info);
+			}
 		},
 		messagesCount: function() {
 			this.$store.dispatch("ad_messagesCount").then(() => {
@@ -239,6 +239,7 @@ export default {
 		loadRooms: function() {
 			this.$store.dispatch("ad_loadRoom").then(() => {
 				this.rooms = this.$store.getters.ad_rooms;
+				this.recentRoom = this.rooms.rooms;
 				const data = {
 					detail: "Phòng",
 					count: this.rooms.count,
@@ -247,13 +248,27 @@ export default {
 				this.info.push(data);
 			});
 		},
-		activeUser: function(id) {
+		setupActiveUser: function(chat) {
+			this.dialogActive = true;
+			this.activeUserId = chat.id;
+			this.activeTitle = chat.active;
+		},
+		activeUser: function() {
+			const user_id = this.activeUserId;
 			this.recent.filter(function(data) {
-				if (data.id === id) {
+				if (data.id === user_id) {
 					data.active = !data.active;
 				}
 			});
-			this.$store.dispatch("ad_disableUser", id).catch((error) => console.log("ACTIVE USER ERROR", error));
+
+			this.$store
+				.dispatch("ad_disableUser", user_id)
+				.then((response) => {
+					if (response.name === "Error") return;
+					console.log("response", response);
+				})
+				.catch((error) => console.log("ACTIVE USER ERROR", error));
+
 			this.dialogActive = false;
 		},
 		textAvatar(roomName) {
@@ -264,8 +279,16 @@ export default {
 				return (words[0][0] + words[words.length - 1][0]).toUpperCase();
 			}
 		},
-		deleteRoom(id) {
-			this.$store.dispatch("ad_deleteRoom", id);
+		setupActiveRoom: function(room) {
+			this.dialogActiveRoom = true;
+			this.activeRoomId = room.id;
+			console.log(room)
+		},
+		deleteRoom() {
+			const roomId = this.activeRoomId;
+			this.$store.dispatch("ad_deleteRoom", roomId);
+			let i = this.recentRoom.map(item => item.id).indexOf(roomId);
+			this.recentRoom.splice(i, 1);
 			this.dialogActiveRoom = false;
 		},
 		getUserData() {
